c92a0c31bff129947df05f186a3c993aad4add7c
Sean <seanzzz@Seans-Mac-Studio.local>
2023-04-28 11:37:04 -0600

    connect client and server, update root package.json with install and start commands

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+
+const planetsRouters = require("./routes/planets/planets.router.js");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
+app.use(planetsRouters);
 
 module.exports = app;
