9da03a55948cedd732a5beb183af632714749c97
seanarch <zhuqingyun@outlook.com>
2023-04-29 21:28:56 -0600

    update package.json

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -11,6 +12,11 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouters);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
