a956251cc0d14e57ed316f8aef53749d58db8c57
seanarch <zhuqingyun@outlook.com>
2023-05-02 13:51:50 -0600

    Update basic post test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -11,7 +11,37 @@ describe("Test GET /launches", () => {
 });
 
 describe("Test POST /launches", () => {
-  test("It should respond with 200 success", () => {});
+  const completeLaunchData = {
+    mission: "USS",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+    launchDate: "January 4, 2028",
+  };
+
+  const launchDataWithoutDate = {
+    mission: "USS",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+  };
+
+  test("It should respond with 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send({
+        mission: "USS",
+        rocket: "NCC 1701-D",
+        target: "Kepler-186 f",
+        launchDate: "January 4, 2028",
+      })
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    expect(responseDate).toBe(requestDate);
+
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
 
   test("It should catch missing required proterties", () => {});
   test("It should catch invalid dates", () => {});
