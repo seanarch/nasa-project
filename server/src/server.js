13a8d1a2320b49614dded226f20672a4728bcda0
seanarch <zhuqingyun@outlook.com>
2023-05-04 12:13:17 -0600

    Create mongodb and added connection, save the url to .env

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,9 @@
 const http = require("http");
+const mongoose = require("mongoose");
+const path = require("path");
+const dotenv = require("dotenv").config({
+  path: path.resolve(__dirname, "../../.env"),
+});
 
 const app = require("./app");
 
@@ -6,9 +11,20 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = process.env.MONGO_URI;
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 }
 
