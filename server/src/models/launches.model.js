94eb39630e0fde46134ca43c81e0bde814723dcf
seanarch <zhuqingyun@outlook.com>
2023-05-06 17:30:58 -0600

    Using findOneAndUpdate in launchesdatabase

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -53,7 +53,7 @@ async function saveLaunch(launch) {
     throw new Error("No matching planet found");
   }
 
-  await launchesDatabase.updateOne(
+  await launchesDatabase.findOneAndUpdate(
     {
       flightNumber: launch.flightNumber,
     },
