732f8a6563a073ccb8d8d8fc1230432b31cfaad5
seanarch <zhuqingyun@outlook.com>
2023-05-06 16:58:01 -0600

    maintain planet integrity

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,6 @@
 const launches = new Map();
 const launchesDatabase = require("./launches.mongo");
+const planets = require("./planets.mongo");
 
 let latestFlightNumber = 100;
 
@@ -34,6 +35,14 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error("No matching planet found");
+  }
+
   await launchesDatabase.updateOne(
     {
       flightNumber: launch.flightNumber,
