cadb6c7d9096e1a545f55ad863e6ba120e0cd263
seanarch <zhuqingyun@outlook.com>
2023-05-06 12:01:14 -0600

    Update launch schema

--- server/src/server.js
+++ server/src/server.js
@@ -26,10 +26,10 @@ mongoose.connection.on("error", (err) => {
 async function startServer() {
   await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
-}
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
 
 startServer();
