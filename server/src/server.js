8ba4f0ac746717dc490b12ae9ce9da23407dde16
seanarch <zhuqingyun@outlook.com>
2023-05-09 14:51:53 -0600

    Added loadlaunchdata method

--- server/src/server.js
+++ server/src/server.js
@@ -6,8 +6,8 @@ const dotenv = require("dotenv").config({
 });
 
 const app = require("./app");
-
 const { loadPlanetsData } = require("./models/planets.model");
+const { loadLaunchData } = require("./models/launches.model");
 
 const PORT = process.env.PORT || 8000;
 
@@ -16,6 +16,7 @@ const server = http.createServer(app);
 async function startServer() {
   await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData();
 
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
