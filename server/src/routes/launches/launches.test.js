6484e0b22b641c90daf6fee91e5b9442dafa7836
seanarch <zhuqingyun@outlook.com>
2023-05-09 13:36:53 -0600

    Update test method with v1 api

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -14,7 +14,7 @@ describe("Launches API", () => {
   describe("Test GET /launches", () => {
     test("It should respond with 200 success", async () => {
       const response = await request(app)
-        .get("/launches")
+        .get("/v1/launches")
         .expect("Content-Type", /json/)
         .expect(200);
     });
@@ -43,7 +43,7 @@ describe("Launches API", () => {
 
     test("It should respond with 201 created", async () => {
       const response = await request(app)
-        .post("/launches")
+        .post("/v1/launches")
         .send({
           mission: "USS",
           rocket: "NCC 1701-D",
@@ -62,7 +62,7 @@ describe("Launches API", () => {
 
     test("It should catch missing required proterties", async () => {
       const response = await request(app)
-        .post("/launches")
+        .post("/v1/launches")
         .send(launchDataWithoutDate)
         .expect("Content-Type", /json/)
         .expect(400);
@@ -73,7 +73,7 @@ describe("Launches API", () => {
     });
     test("It should catch invalid dates", async () => {
       const response = await request(app)
-        .post("/launches")
+        .post("/v1/launches")
         .send(launchDataWithInvalidDate)
         .expect("Content-Type", /json/)
         .expect(400);
