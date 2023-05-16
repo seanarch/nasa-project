dbe1cb97e8b289177ffd370698caf9fc33496d84
seanarch <zhuqingyun@outlook.com>
2023-05-16 14:58:07 -0600

    Added launch pagination

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -117,8 +117,11 @@ async function abortLaunchById(launchId) {
   return aborted.modifiedCount === 1;
 }
 
-async function getAllLaunches() {
-  return await launchesDatabase.find({}, { _id: 0, __v: 0 });
+async function getAllLaunches(skip, limit) {
+  return await launchesDatabase
+    .find({}, { _id: 0, __v: 0 })
+    .skip(skip)
+    .limit(limit);
 }
 
 async function saveLaunch(launch) {
