fef49cae9df91fa45cd090c0781892a62c271c8c
seanarch <zhuqingyun@outlook.com>
2023-05-19 17:33:49 -0600

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,10 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 
-render(<App />, document.getElementById("root"));
+const root = document.getElementById("root");
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  root
+);
